136d96e827bf94de97adbd697a3e30dd5254d5fc
wildhippogrif <196724648+wildhippogrif@users.noreply.github.com>
2025-10-04 00:40:52 -0400

    Finished Lab 4

--- EEL3135 (Signals & Systems)/Lab 4/EEL_3135_Lab_4_Template.m
+++ EEL3135 (Signals & Systems)/Lab 4/EEL_3135_Lab_4_Template.m
@@ -1,3 +1,7 @@
+clc;
+clear all;
+close all;
+
 %% EEL 3135 Lab 4 
 % Name:Evan Baesler
 % Date: 09-24-2025
@@ -88,15 +92,28 @@ grid on;
 
 figure;
 stem(Freqs, angle(A_k), 'filled');
-title('Magnitude Spectrum |A_k|');
+title('Frequency Spectrum |A_k|');
 xlabel('Frequency (Hz)');
 ylabel('|A_k');
 grid on;
 
 % Question 9
 
+threshold = 1/10000;  % small number incase of rounding from numerical methods
+Freq_nonzero = [];  
+
+for n = 1:length(A_k)
+    if abs(A_k(n)) > threshold
+        Freq_nonzero(end+1) = Freqs(n);
+    end
+end
+
 % Question 10
 
+% By definition, an nth harmonic is n times faster than the first harmonic.
+% This is because in an array of harmonics, the fundamental frequency is
+% your step. For every step in the same direction, you are moving n times 
+% faster than the first harmonic.
 
 %% Part 3 Fourier Series (Synthesis)
 
@@ -111,27 +128,89 @@ grid on;
 % WARNING: IF max(imag(ReconstructedSignal)) RETURNS A VALUE LARGER THAN
 % 10^-10,YOUR CODE IS NOT CORRECT!
 
-%K = ??;
-%for  k = -K:K  
-    
-%end
+ReconstructedSignal = zeros(size(T_s));
 
+n = 1;
+
+for k = -K:K
+
+     ReconstructedSignal = ReconstructedSignal + A_k(n) * exp(1i*2*pi*k*f_0*T_s);
+     n = n+1;
+
+end
+
+max(imag(ReconstructedSignal)); % returns 2.2204e-15
+
+ReconstructedSignal = real(ReconstructedSignal);
 
 % Question 12
 % Hint: help hold might be useful to type in your command window 
 % Exploring the matlab plot legend and color options for plots 
 % might make it easier to read. Be sure to explain what your data.
 
+plot(T_s,Y_t,'r')% T_s, ReconstructedSignal, 'b--');
+hold on;
+grid on;
 
 % Question 13
 
 % Improved attempt at analysis goes here
 % Hint: What could we do to better analyze the signal?
 
+% How I will improve: Lets remove rounding and increase harmonics captured.
 
 % Improved attempt at synthesis goes here
+
+K = 30;
+
+Kernel_R = zeros(2*K+1,L_T_o);
+Kernel_I = zeros(2*K+1,L_T_o);
+
+n=1;
+for  k = -K:K  
+     Kernel_R(n,:) = Y_t(1:L_T_o) .* cos(2*pi*k*f_0*T_s(1:L_T_o));
+     Kernel_I(n,:) = Y_t(1:L_T_o) .* sin(2*pi*k*f_0*T_s(1:L_T_o));
+     A_R_k(n) = (1/T_0)*trapz(T_s(1:L_T_o),Kernel_R(n, :));   % Real 
+     A_I_k(n) = (1/T_0)*trapz(T_s(1:L_T_o),Kernel_I(n, :));   % Imaginary
+     A_k(n)= A_R_k(n)+i*A_I_k(n);
+     n=1+n; 
+end
+
+Freqs = (-K:K) * (1/T_0);
+
+threshold = 1/10000;  % small number incase of rounding from numerical methods
+Freq_nonzero = [];  
+
+for n = 1:length(A_k)
+    if abs(A_k(n)) > threshold
+        Freq_nonzero(end+1) = Freqs(n);
+    end
+end
+
+ReconstructedSignal = zeros(size(T_s));
+
+n = 1;
+
+for k = -K:K
+
+     ReconstructedSignal = ReconstructedSignal + A_k(n) * exp(1i*2*pi*k*f_0*T_s); % DC
+     n = n+1;
+
+end
+
+max(imag(ReconstructedSignal)); % returns 2.2204e-15
+
+ReconstructedSignal = real(ReconstructedSignal);
+
+plot(T_s, ReconstructedSignal, 'c')
+legend;
 
 
 
 % Question 14 
 % For full points, provide more than a single sentence.
+
+% To get better accuracy, I used more than the 10 origianlly provided
+% harmonics. This allowed me to make sure my amplitudes matched better by
+% taking the contributions from latter harmonics, even if they do not add
+% much, the sum of their contributions can add up.
